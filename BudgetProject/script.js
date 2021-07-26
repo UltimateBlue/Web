cb9a79d29d12cf220ceb0332012032173ab3fe70
UltimateBlue <ultimatebluee@gmail.com>
2021-07-26 23:43:57 +0330

    adding records is done, next we will do deleting records!

--- BudgetProject/script.js
+++ BudgetProject/script.js
@@ -1,63 +1,92 @@
 
 let UIController = (function () {
 
+    let DOMStrings = {
+        incList: 'inc-list',
+        currentIncome: 'current-income',
+        expList: 'exp-list',
+        currentExpense: 'current-expense',
+        expenseTotalPerc: 'expense-total-perc',
+        currentAmount: 'current-amount',
+        descript: 'descript',
+        moneyAmount: 'money-amount',
+        plusMinus: 'plus-minus',
+        tik: 'tik',
+        inc: 'inc',
+    };
+
+
     let addIncRecord = function (newRecord) {
-        let html = '<div id="income-%id%" class="list-items"><div class= "income-des"><p>%descript%</p></div><div class="income-val"><p>+%value%</p><svg class="del-item" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M347.94 129.86L203.6 195.83a31.938 31.938 0 0 0-15.77 15.77l-65.97 144.34c-7.61 16.65 9.54 33.81 26.2 26.2l144.34-65.97a31.938 31.938 0 0 0 15.77-15.77l65.97-144.34c7.61-16.66-9.54-33.81-26.2-26.2zm-77.36 148.72c-12.47 12.47-32.69 12.47-45.16 0-12.47-12.47-12.47-32.69 0-45.16 12.47-12.47 32.69-12.47 45.16 0 12.47 12.47 12.47 32.69 0 45.16zM248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm0 448c-110.28 0-200-89.72-200-200S137.72 56 248 56s200 89.72 200 200-89.72 200-200 200z" /></svg></div></div>';
+        let html = '<div id="income-%id%" class="list-items"><div class= "income-des"><p>%descript%</p></div><div class="income-val"><p>+%value%</p><svg class="del-item" id="del-inc-item%id%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M347.94 129.86L203.6 195.83a31.938 31.938 0 0 0-15.77 15.77l-65.97 144.34c-7.61 16.65 9.54 33.81 26.2 26.2l144.34-65.97a31.938 31.938 0 0 0 15.77-15.77l65.97-144.34c7.61-16.66-9.54-33.81-26.2-26.2zm-77.36 148.72c-12.47 12.47-32.69 12.47-45.16 0-12.47-12.47-12.47-32.69 0-45.16 12.47-12.47 32.69-12.47 45.16 0 12.47 12.47 12.47 32.69 0 45.16zM248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm0 448c-110.28 0-200-89.72-200-200S137.72 56 248 56s200 89.72 200 200-89.72 200-200 200z" /></svg></div></div>';
 
         let newHtml;
         newHtml = html.replace('%id%', newRecord.id);
-        newHtml = newHtml.replace('%descript%', newRecord.descript);
+        newHtml = newHtml.replace('%descript%', newRecord.desc);
         newHtml = newHtml.replace('%value%', newRecord.val);
 
-
-        document.getElementById('inc-list').insertAdjacentHTML('afterbegin', newHtml);
-
-        document.getElementById('current-income').innerHTML = '';
-        document.getElementById('current-income').insertAdjacentText('afterbegin', newRecord.totalIncome.toFixed(2));
-
-        return 1;
+        document.getElementById(DOMStrings.incList).insertAdjacentHTML('afterbegin', newHtml);
     };
 
     let addExpRecord = function (newRecord) {
-        let html = '<div id="exp-%id%" class="list-items"><div class="exp-des"><p>%descript%</p></div><div class="exp-val">   <p>-%value%</p><div class="exp-percent">%perc%%</div><svg class="del-item" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M347.94 129.86L203.6 195.83a31.938 31.938 0 0 0-15.77 15.77l-65.97 144.34c-7.61 16.65 9.54 33.81 26.2 26.2l144.34-65.97a31.938 31.938 0 0 0 15.77-15.77l65.97-144.34c7.61-16.66-9.54-33.81-26.2-26.2zm-77.36 148.72c-12.47 12.47-32.69 12.47-45.16 0-12.47-12.47-12.47-32.69 0-45.16 12.47-12.47 32.69-12.47 45.16 0 12.47 12.47 12.47 32.69 0 45.16zM248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm0 448c-110.28 0-200-89.72-200-200S137.72 56 248 56s200 89.72 200 200-89.72 200-200 200z"/></svg></div></div>';
+        let html = '<div id="exp-%id%" class="list-items"><div class="exp-des"><p>%descript%</p></div><div class="exp-val">   <p>-%value%</p><div class="exp-percent">%perc%%</div><svg class="del-item" id="del-exp-item%id% xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M347.94 129.86L203.6 195.83a31.938 31.938 0 0 0-15.77 15.77l-65.97 144.34c-7.61 16.65 9.54 33.81 26.2 26.2l144.34-65.97a31.938 31.938 0 0 0 15.77-15.77l65.97-144.34c7.61-16.66-9.54-33.81-26.2-26.2zm-77.36 148.72c-12.47 12.47-32.69 12.47-45.16 0-12.47-12.47-12.47-32.69 0-45.16 12.47-12.47 32.69-12.47 45.16 0 12.47 12.47 12.47 32.69 0 45.16zM248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm0 448c-110.28 0-200-89.72-200-200S137.72 56 248 56s200 89.72 200 200-89.72 200-200 200z"/></svg></div></div>';
 
         let newHtml;
         newHtml = html.replace('%id%', newRecord.id);
-        newHtml = newHtml.replace('%descript%', newRecord.descript);
+        newHtml = newHtml.replace('%descript%', newRecord.desc);
         newHtml = newHtml.replace('%value%', newRecord.val);
-        newHtml = newHtml.replace('%perc%', newRecord.recordPerc.toFixed(0));
+        newHtml = newHtml.replace('%perc%', newRecord.perc.toFixed(0));
 
-        document.getElementById('exp-list').insertAdjacentHTML('afterbegin', newHtml);
-
-        document.getElementById('current-expense').innerHTML = '';
-        document.getElementById('current-expense').insertAdjacentText('afterbegin', newRecord.totalExpense.toFixed(2));
-        document.getElementById('expense-total-perc').innerHTML = '';
-        document.getElementById('expense-total-perc').insertAdjacentText('afterbegin', newRecord.totalPerc.toFixed(0));
-
-        return 1;
+        document.getElementById(DOMStrings.expList).insertAdjacentHTML('afterbegin', newHtml);
     };
 
     let updateHeader = function (newRecord) {
-        document.getElementById('current-amount').innerHTML = '';
-        document.getElementById('current-amount').insertAdjacentText('afterbegin', newRecord.totalAsset.toFixed(2));
+        document.getElementById(DOMStrings.currentAmount).innerHTML = '';
+        document.getElementById(DOMStrings.currentAmount).insertAdjacentText('afterbegin', newRecord[1].toFixed(2));
 
-        
+        document.getElementById(DOMStrings.currentIncome).innerHTML = '';
+        document.getElementById(DOMStrings.currentIncome).insertAdjacentText('afterbegin', newRecord[2].toFixed(2));
 
+        document.getElementById(DOMStrings.currentExpense).innerHTML = '';
+        document.getElementById(DOMStrings.currentExpense).insertAdjacentText('afterbegin', newRecord[3].toFixed(2));
+        document.getElementById(DOMStrings.expenseTotalPerc).innerHTML = '';
+        document.getElementById(DOMStrings.expenseTotalPerc).insertAdjacentText('afterbegin', newRecord[4].toFixed(0));
 
     };
 
     return {
-        updateUI: function (newRecord) {
+        updateUI: function (changes, sign) {
             // console.log('update UI');    ==> OK
 
             let done = 0;
-            if (newRecord.sign === 'inc') {
-                done = addIncRecord(newRecord);
+            if (sign === 'inc') {
+                done = addIncRecord(changes[0]);
             } else {
-                done = addExpRecord(newRecord);
+                done = addExpRecord(changes[0]);
             }
-            updateHeader(newRecord);
+            updateHeader(changes);
             return done;
+        },
+
+        getDOM: function () {
+            return DOMStrings;
+        },
+
+        clearFields: function () {
+
+            // my code, more simpler
+            document.getElementById(DOMStrings.descript).value = '';
+            document.getElementById(DOMStrings.moneyAmount).value = '';
+            document.getElementById(DOMStrings.descript).focus();
+
+
+            // tutor's code, with tutorial purposes
+            // let fieldArr, fields;
+            // fields = document.querySelectorAll(DOMStrings.descript+','+DOMStrings.moneyAmount);
+            // fieldArr = Array.prototype.slice.call(fields);
+            // fieldArr.forEach(function(current, index, array){
+            //     current.value='';
+            // });
+            // fieldArr[0].focus();
         },
     }
 
@@ -68,80 +97,77 @@ let UIController = (function () {
 
 let DataController = (function () {
 
-    let totalAsset = 0;
-    let totalIncome = 0;
-    let totalExpense = 0;
-    let totalPerc = 0;
-    let records = [];
-    let incNum = 0;
-    let expNum = 0;
+    let db = {
+        records: {
+            incs: [],
+            exps: [],
+        },
+        totalPerc: 0,
+        totalAsset: 0,
+        totalIncome: 0,
+        totalExpense: 0,
+    };
 
 
-
-
-    let procNewIncome = function (newRecord) {
+    let procNewIncome = function (descript, val) {
         // console.log('its an income');    ==> OK
-        totalAsset = totalAsset + parseInt(newRecord.val);
-        newRecord.totalAsset = totalAsset;
-        totalIncome = totalIncome + parseInt(newRecord.val);
-        newRecord.totalIncome = totalIncome;
-        incNum = incNum + 1;
-        newRecord.id = incNum;
+        let record = {
+            id: 0,
+            desc: '',
+            val: 0
+        };
 
-        return newRecord;
+
+        db.totalAsset = db.totalAsset + val;
+        db.totalIncome = db.totalIncome + val;
+        if (db.records.incs.length > 0) {
+            record.id = db.records.incs[db.records.incs.length - 1].id + 1;
+        } else {
+            record.id = 0;
+        }
+        record.desc = descript;
+        record.val = val;
+
+        db.records.incs.push(record);
+        return [record, db.totalAsset, db.totalIncome, db.totalExpense, db.totalPerc];
 
     };
 
-    let procNewExpense = function (newRecord) {
-        // console.log('its an expense');   ==> OK
-        totalAsset = totalAsset - parseInt(newRecord.val);
-        newRecord.totalAsset = totalAsset;
-        totalExpense = totalExpense + parseInt(newRecord.val);
-        newRecord.totalExpense = totalExpense;
-        totalPerc = (totalExpense * 100) / totalIncome;
-        newRecord.totalPerc = totalPerc;
-        recordPerc = (newRecord.val * 100) / totalAsset;
-        newRecord.recordPerc = recordPerc;
-        expNum = expNum + 1;
-        newRecord.id = expNum;
+    let procNewExpense = function (descript, val) {
+        let record = {
+            id: 0,
+            desc: '',
+            val: 0,
+            perc: 0
+        };
 
-        return newRecord;
+        db.totalAsset = db.totalAsset - val;;
+        db.totalExpense = db.totalExpense + val;
+        db.totalPerc = Math.round((db.totalExpense * 100) / db.totalIncome);
+                
+        if (db.records.exps.length > 0) {
+            record.id = db.records.exps[db.records.exps.length - 1].id + 1;
+        } else {
+            record.id = 0;
+        }
+        record.desc = descript;
+        record.val = val;
+        record.perc = Math.round((val * 100) / db.totalIncome);
 
+        db.records.exps.push(record);
+        return [record, db.totalAsset, db.totalIncome, db.totalExpense, db.totalPerc];
     };
 
 
     // API part
     return {
         newValue: function (sign, descript, val) {
-            let newRecord = {
-                id: 0,
-                sign: sign,
-                descript: descript,
-                val: val,
-                totalPerc: 0,
-                recordPerc: 0,
-                totalAsset: 0,
-                totalIncome: 0,
-                totalExpense: 0,
-                totalPerc
-            };
-
             if (sign === 'inc') {
-                newRecord = procNewIncome(newRecord);
+                changes = procNewIncome(descript, val);
             } else {
-                newRecord = procNewExpense(newRecord);
+                changes = procNewExpense(descript, val);
             }
-
-            // //TEMP PART BEGIN
-            //     newRecord = {
-            //         sign: 1,
-            //         desc: 'description',
-            //         val: 100
-            //     };
-
-            // //TEMP PART END
-            records.push(newRecord);
-            return newRecord;
+            return changes;
         },
     }
 
@@ -151,40 +177,47 @@ let DataController = (function () {
 
 let Controller = (function (dataCTRL, uiCTRL) {
 
+    let DOMStrings = uiCTRL.getDOM();
+
     let getInput = function () {
 
-        //ADD EVENT HANDLERS
-        let descript = document.getElementById('descript').value;
-        let val = document.getElementById('money-amount').value;
-        let sign = document.getElementById('plus-minus').value;
-        // console.log(descript+val+sign); // ==> OK
+        // GET UI FIELDS DATA
+        let descript = document.getElementById(DOMStrings.descript).value;
+        let val = parseFloat(document.getElementById(DOMStrings.moneyAmount).value);
+        let sign = document.getElementById(DOMStrings.plusMinus).value;
 
-        let newRecord = dataCTRL.newValue(sign, descript, val);
-        console.log(newRecord);
-        let done = uiCTRL.updateUI(newRecord);
-        if (done === 1) {
-            console.log('its done!');
+        if (isNaN(val) || val <= 0 || descript === "") {
+            return;
         }
+
+        // INSERT THE NEW RECORD IN THE DATABASE
+        let changes = dataCTRL.newValue(sign, descript, val);
+
+        // UPDATE UI
+        uiCTRL.updateUI(changes,sign);
+
+        // CLEAR INPUT FILEDS
+        uiCTRL.clearFields();
 
     };
 
+    let delItem = function () {
+        console.log('hi');
+    };
 
-
-
-
-    // event handlers
-    document.getElementById('tik').addEventListener('click', getInput);
-    document.addEventListener('keypress', function (e) {
-        if (e.key === 13 || e.which === 13 || e.keyCode === 13) {
-            getInput();
-        }
-    });
-
-
-
-
-
-
+    let init = (function () {
+        // ADD EVENT HANDLERS
+        document.getElementById(DOMStrings.tik).addEventListener('click', getInput);
+        document.addEventListener('keypress', function (e) {
+            if (e.key === 13 || e.which === 13 || e.keyCode === 13) {
+                getInput();
+            }
+        });
+        // document.querySelector('.del-item').addEventListener('click', function(e){
+        //     delItem();
+        //     console.log(e);
+        // });
+    })();
 
 })(DataController, UIController);
 
