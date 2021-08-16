c3ad0fc2ad167c73d420d9b71a2bae8552c4b5b7
UltimateBlue <ultimatebluee@gmail.com>
2021-08-16 16:39:45 +0330

    modification, adding blur appearance - in order to learn

--- 002-modal-window/script.js
+++ 002-modal-window/script.js
@@ -1,13 +1,19 @@
 document.querySelector('.close-btn').addEventListener('click', closeModalWin);
-document.querySelector('.divs').addEventListener('click', openModalWin);
+document.querySelectorAll('.divs').forEach(curr => curr.addEventListener('click', openModalWin));
+document.querySelector('.blury').addEventListener('click', closeModalWin);
 
-
+document.querySelector('.blury').style.display = 'none';
+document.querySelector('.hidden-modal-window').style.display = 'none';
+// document.querySelector('body').classList.toggle('blury');
 
 function closeModalWin() {
-    document.querySelector('.fixed-fullscreen-div').style.display = 'none';
+    document.querySelector('.hidden-modal-window').style.display = 'none';
+    document.querySelector('.blury').style.display = 'none';
 }
 
 function openModalWin() {
-    document.querySelector('.fixed-fullscreen-div').style.display = 'flex';
+    document.querySelector('.hidden-modal-window').style.display = 'flex';
+    document.querySelector('.blury').style.display = 'block';
+
 
 }
