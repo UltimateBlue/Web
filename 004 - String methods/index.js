8265f55625e02f871c1b9a98f6fcd1962b3d6fde
UltimateBlue <ultimatebluee@gmail.com>
2021-09-05 00:01:27 +0330

    training for string methods - learning

---
+++ 004 - String methods/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+document.querySelector('button').addEventListener('click', btnHandler);
+
+function btnHandler() {
+    const input_data = document.querySelector('textarea').value;
+
+    let ls = input_data.split('\n');
+
+    const newlist = ls.map(curr => {
+        let newWord = '';
+        curr.trim().split('_').forEach(cur => { newWord += cur[0].toUpperCase() + cur.slice(1) });
+        console.log(`${newWord}`);
+
+    })
+}
